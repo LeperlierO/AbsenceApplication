8d739b8580ba9632dc552b2688f2909eec9c513c
LeperlierO <leperlierolivier3@gmail.com>
2024-02-13 10:34:57 +0100

    binding converter with interface ivalueconverter

---
+++ AbsenceApplication/Tools/MyConverters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AbsenceApplication.Tools
+{
+    // Convertisseurs qui vont permettre de modifier les données à afficher au binding sur les template
+    public class MyConverters : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Du code vers l'interface
+            if(value != null && value is string)
+            {
+                string name = value as string;
+                return name.ToUpper();
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Interface vers le code
+            return value;
+        }
+    }
+}
